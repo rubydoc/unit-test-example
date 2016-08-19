6aef058b205e1b837a25302676c97351a9054ced
Kim SeongJun <me@kimseongjun.co.kr>
2016-08-19 15:30:40 +0900

    [Step3] Change to call PhoneNumber#to_formatted in Guy [Step4] Add new apect to PhoneNumber#to_formatted

--- app/services/guy.rb
+++ app/services/guy.rb
@@ -1,6 +1,5 @@
 class Guy
   def give_my_number_to girl
-    "010-5585-0440"
-    # PhoneNumber.new("01055850440").to_formatted
+    PhoneNumber.new("01055850440", "+82").to_formatted
   end
 end
--- spec/services/phone_number_spec.rb
+++ spec/services/phone_number_spec.rb
@@ -11,5 +11,14 @@ RSpec.describe PhoneNumber, :type => :service do
         expect(phone_number.to_formatted).to eq "010-1234-5678"
       end
     end
+    
+    context "when receiving the mobile number '011xxxxxxx'" do
+      let(:phone_number) { PhoneNumber.new("0111234567", "+82") }
+      
+      it "should return '011-xxx-xxxx' format" do
+        expect(phone_number.to_formatted).to eq "011-123-4567"
+      end
+    end
   end
+  
 end
