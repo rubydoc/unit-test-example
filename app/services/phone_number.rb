09fb15090eb6ea13180a641d998265421e83f61e
Kim SeongJun <me@kimseongjun.co.kr>
2016-08-19 16:00:38 +0900

    [Step11] Support 02-xxx-xxxx type

--- app/services/phone_number.rb
+++ app/services/phone_number.rb
@@ -9,8 +9,8 @@ class PhoneNumber
       if @number.length > 10
         split_by_point(3,7)
       else
-        if @number[0...2] == "02"
-          split_by_point(2,6)
+        if is_seoul? 
+          seoul_number_formatted
         else
           split_by_point(3,6)
         end
@@ -25,5 +25,17 @@ class PhoneNumber
 
   def split_by_point(first, second)
     "#{@number[0...first]}-#{@number[first...second]}-#{@number[second..-1]}"
+  end
+  
+  def is_seoul?
+    @number[0...2] == "02"
+  end
+  
+  def seoul_number_formatted
+    if @number.length > 9
+      split_by_point(2,6)
+    else
+      split_by_point(2,5)
+    end
   end
 end
