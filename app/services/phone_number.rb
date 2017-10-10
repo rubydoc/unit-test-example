7553ba39c6e28fa99e4402a31a623ef71442b65c
Kim SeongJun <me@kimseongjun.co.kr>
2017-10-10 12:23:58 +0900

    [Step14] Refactor PhoneNumber#to_formatted with confidence

--- app/services/phone_number.rb
+++ app/services/phone_number.rb
@@ -3,39 +3,30 @@ class PhoneNumber
     @number = number
     @country = country
   end
-  
+
   def to_formatted
     if @country == "ko"
-      if @number.length > 10
-        split_by_point(3,7)
-      else
-        if is_seoul?
-          seoul_number_formatted
-        else
-          split_by_point(3,6)
-        end
-      end
+      split_by_point(first_splitter, second_splitter)
     else
       "not available"
     end
-    
   end
-  
+
   private
 
   def split_by_point(first, second)
     "#{@number[0...first]}-#{@number[first...second]}-#{@number[second..-1]}"
   end
-  
+
   def is_seoul?
     @number[0...2] == "02"
   end
-  
-  def seoul_number_formatted
-    if @number.length > 9
-      split_by_point(2,6)
-    else
-      split_by_point(2,5)
-    end
+
+  def first_splitter
+    is_seoul? ? 2 : 3
+  end
+
+  def second_splitter
+    @number.length - 4
   end
 end
